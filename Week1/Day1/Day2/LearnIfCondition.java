a8d41e50e9098a82ccbbdf4ef1937a26001d2882
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-07-17 15:55:37 +0530

    Add files via upload

---
+++ Week1/Day1/Day2/LearnIfCondition.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class LearnIfCondition {
+
+	public static void main(String[] args) {
+		
+		
+		int x=100;
+		int y=100;
+		
+		if(x>y) {
+			System.out.println("X is greater than Y");
+			}
+		
+		else {
+		System.out.println("Y is Greater than X");
+		}
+
+}
+}
---
+++ Week1/Day1/Day2/LearnSwitchCase.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class LearnSwitchCase {
+
+	public static void main(String[] args) {
+	
+		String browser="chrome";
+		switch("chrome") {
+		
+		case "chrome":
+		System.out.println("Chrome");
+		break;
+		
+		case "firefox":
+		System.out.println("Firefox");
+		break;
+		
+		case "Edge":
+		System.out.println("Edge");
+		break;
+		
+	}	
+
+	}
+
+}
